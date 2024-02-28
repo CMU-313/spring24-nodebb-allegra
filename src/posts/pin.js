e643eecc89e8c93fbe5564a6cbac26aee4ada2cf
Albert Lin <albert1104@gmail.com>
2024-02-28 15:54:07 -0500

    Added new file pin.js for backend implementation to mark post as pinned

---
+++ src/posts/pin.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const _ = require('lodash');
+
+const db = require('../database');
+const topics = require('../topics');
+const categories = require('../categories');
+const user = require('../user');
+const notifications = require('../notifications');
+const plugins = require('../plugins');
+const flags = require('../flags');
+
+module.exports = function (Posts) {
+    
+}

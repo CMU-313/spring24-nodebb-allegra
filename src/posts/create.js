5bf90202f71c6e3f6f68ff36cd28a39d98f73614
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2024-02-28 12:12:40 -0500

    Connect backend to front end with anonymous property

--- src/posts/create.js
+++ src/posts/create.js
@@ -1,40 +1,44 @@
-'use strict';
+"use strict";
 
-const _ = require('lodash');
+const _ = require("lodash");
 
-const meta = require('../meta');
-const db = require('../database');
-const plugins = require('../plugins');
-const user = require('../user');
-const topics = require('../topics');
-const categories = require('../categories');
-const groups = require('../groups');
-const utils = require('../utils');
+const meta = require("../meta");
+const db = require("../database");
+const plugins = require("../plugins");
+const user = require("../user");
+const topics = require("../topics");
+const categories = require("../categories");
+const groups = require("../groups");
+const utils = require("../utils");
 
 module.exports = function (Posts) {
     Posts.create = async function (data) {
-        const { uid, tid, isAnonymous } = data; 
+        const { uid, tid, isAnonymous: anonymous } = data;
         // Here, we reverse the logic: append the text if the post is NOT anonymous
-        const content = isAnonymous ? data.content.toString() : data.content.toString() + " [This post was made anonymously]";
+        const content = anonymous
+            ? data.content.toString()
+            : data.content.toString() +
+              " [This post was made anonymously]" +
+              "${data}";
         const timestamp = data.timestamp || Date.now();
         const isMain = data.isMain || false;
 
         if (!uid && parseInt(uid, 10) !== 0) {
-            throw new Error('[[error:invalid-uid]]');
+            throw new Error("[[error:invalid-uid]]");
         }
 
         if (data.toPid && !utils.isNumber(data.toPid)) {
-            throw new Error('[[error:invalid-pid]]');
+            throw new Error("[[error:invalid-pid]]");
         }
 
-        const pid = await db.incrObjectField('global', 'nextPid');
+        const pid = await db.incrObjectField("global", "nextPid");
         let postData = {
             pid: pid,
             uid: uid, // Keep the actual UID since now we are not specifically handling anonymous logic here
             tid: tid,
             content: content,
             timestamp: timestamp,
-            isAnonymous: !!isAnonymous, // Still store the actual state of isAnonymous
+            isAnonymous: !!anonymous, // Still store the actual state of isAnonymous
         };
 
         if (data.toPid) {
@@ -47,16 +51,19 @@ module.exports = function (Posts) {
             postData.handle = data.handle;
         }
 
-        let result = await plugins.hooks.fire('filter:post.create', { post: postData, data: data });
+        let result = await plugins.hooks.fire("filter:post.create", {
+            post: postData,
+            data: data,
+        });
         postData = result.post;
         await db.setObject(`post:${postData.pid}`, postData);
 
-        const topicData = await topics.getTopicFields(tid, ['cid', 'pinned']);
+        const topicData = await topics.getTopicFields(tid, ["cid", "pinned"]);
         postData.cid = topicData.cid;
 
         await Promise.all([
-            db.sortedSetAdd('posts:pid', timestamp, postData.pid),
-            db.incrObjectField('global', 'postCount'),
+            db.sortedSetAdd("posts:pid", timestamp, postData.pid),
+            db.incrObjectField("global", "postCount"),
             user.onNewPostMade(postData),
             topics.onNewPostMade(postData),
             categories.onNewPostMade(topicData.cid, topicData.pinned, postData),
@@ -65,9 +72,12 @@ module.exports = function (Posts) {
             Posts.uploads.sync(postData.pid),
         ]);
 
-        result = await plugins.hooks.fire('filter:post.get', { post: postData, uid: data.uid });
+        result = await plugins.hooks.fire("filter:post.get", {
+            post: postData,
+            uid: data.uid,
+        });
         result.post.isMain = isMain;
-        plugins.hooks.fire('action:post.save', { post: _.clone(result.post) });
+        plugins.hooks.fire("action:post.save", { post: _.clone(result.post) });
 
         return result.post;
     };
@@ -77,8 +87,12 @@ module.exports = function (Posts) {
             return;
         }
         await Promise.all([
-            db.sortedSetAdd(`pid:${postData.toPid}:replies`, timestamp, postData.pid),
-            db.incrObjectField(`post:${postData.toPid}`, 'replies'),
+            db.sortedSetAdd(
+                `pid:${postData.toPid}:replies`,
+                timestamp,
+                postData.pid,
+            ),
+            db.incrObjectField(`post:${postData.toPid}`, "replies"),
         ]);
     }
 };
